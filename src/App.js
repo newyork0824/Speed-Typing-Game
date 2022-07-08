cdd19bbab0463af51ecc39478dd149a173d04d48
newyork0824 <96758572+newyork0824@users.noreply.github.com>
2022-07-07 21:14:22 -0700

    deploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    
+    
+   
+ const[text, setText] = React.useState("") 
+ const[timeLeft, setTimeLeft] = React.useState(10)
+ const[wordCount,setWordCount] = React.useState(0)
+ const[gameIsRunning, setGameIsRunning] = React.useState(false)
+ const [gameHasRan, setGameHasRan] = React.useState(false)
+  const inputRef = React.useRef(null)
+ 
+ React.useEffect(() => {
+ 
+    if(timeLeft > 0 && gameIsRunning) {
+        setGameHasRan(true)
+       setTimeout(() => {
+           setTimeLeft(prevTime => prevTime - 1)
+            
+       },1000)  
+    }else {
+        setTimeLeft(0)
+        setGameIsRunning(false)
+        const numOfWords = countWords(text)
+        setWordCount(numOfWords)
+    
+  
+        
+    }
+           
+          
+       console.log(timeLeft)
+ 
+     
+     
+ },[timeLeft])
+ 
+const startGame = () => {
+    setGameIsRunning(true)
+    setTimeLeft(10)
+    setText("")
+    inputRef.current.focus()
+}
+ 
+function handleChange(event) {
+    const {value} = event.target
+    setText(value)
 }
 
-export default App;
+function countWords(text) {
+    
+    let numOfWords = text.split(" ")
+   return numOfWords.filter(word => word !== "").length
+       
+}
+
+
+
+const startBtn = <button onClick={startGame} className="start-btn">{gameHasRan ? "Play Again" : "Start"}  </button>
+
+
+
+ 
+ 
+ return (
+     <div>
+  <h1> How fast can You Type? </h1>
+  <textarea
+  ref={inputRef}
+  onChange = {handleChange}
+  value={text}
+  readOnly = {gameIsRunning === true ? false : true} 
+  />
+  <h4> Time Remaining: {timeLeft} </h4>
+  {gameIsRunning === false ? startBtn : ""}
+   <h1>  {gameIsRunning === false && timeLeft === 0 ?   `Word Count: ${wordCount}` : ""} </h1>
+  </div>
+     
+     
+     
+     
+     
+     
+ )  
+    
+    
+    
+}
+export default App 
+
+
+
+
+
+
